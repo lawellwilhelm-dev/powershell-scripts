87a91553a7962b0db029fff07f22847991bfe398
lawellWilhelm-dev <lawellwilhelm.dev@gmail.com>
2020-08-09 11:44:02 +0000

    Set 'Windows 10 Enterprise' First Boot Device

--- create-vms.ps1
+++ create-vms.ps1
@@ -5,3 +5,7 @@ Get-VMScsiController -VMName "Windows 10 Enterprise"
 Add-VMScsiController -VMName "Windows 10 Enterprise" 
 
 Add-VMDvdDrive -VMName "Windows 10 Enterprise" -ControllerNumber 1 -ControllerLocation 0 -Path "D:\DataStore\os\18363.418.191007-0143.19h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
+
+$DVDDrive = Get-VMDvdDrive -VMName "Windows 10 Enterprise"
+
+Set-VMFirmware -VMName "Windows 10 Enterprise" -FirstBootDevice  $DVDDrive
