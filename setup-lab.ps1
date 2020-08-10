b6ef0c171bffaa21e10745876c2cf20119ea4cca
lawellWilhelm-dev <lawellwilhelm.dev@gmail.com>
2020-08-10 00:44:01 +0000

    Modify the script to set up each VM's maximum memory size

--- setup-lab.ps1
+++ setup-lab.ps1
@@ -1,3 +1,4 @@
+#CREATE VIRTUAL MACHINES
 # Create an array of objects,each representing a differencing disk for the lab
 $Disks = @(
     [Tuple]::Create("D:\Hyper-V\Virtual Hard Disks\DC.vhdx", "D:\Hyper-V\Virtual Hard Disks\Windows Server 2016 DX.vhdx"),
@@ -24,17 +25,22 @@ foreach ($disk in $Disks) {
     $vm = $VMs[$count];
 
     Write-Verbose -Message ("Creating Differencing disk for " + $vm[0]) -Verbose
-    New-VHD -Path $disk[0] -ParentPath $disk[1] -Differencing -Verbose
+    New-VHD -Path $disk[0] -ParentPath $disk[1] -Differencing
     Write-Verbose -Message "Differencing disk created successfully" -Verbose
 
     # Iterate through $VMs and Create the VMs
     Write-Verbose -Message ("Creating " + $vm[0]) -Verbose
     New-VM -Name $vm[0] -MemoryStartupBytes $vm[1] -Generation 2 -VHDPath $disk[0] -SwitchName intSwitch1
+
+    Write-Verbose -Message ("Setting up the maximum memory size for " + $vm[0]) -Verbose
+    Set-VMMemory -VMName $vm[0] -MaximumBytes $vm[1]
     
     if ($vm[0] -eq "ROUTER") {
-        Add-VMNetworkAdapter -VMName $vm[0] -SwitchName intSwitch2 -Verbose
+        Add-VMNetworkAdapter -VMName $vm[0] -SwitchName intSwitch2
     }
 
     Write-Verbose -Message ($vm[0] + " Created successfully") -Verbose
     $count++;
 }
+
+#SETUP VIRTUAL MACHINES
